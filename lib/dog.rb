8e785a7e74e1d6bc34486d1438701b2f5e9cfb24
Eric Johnson <johnson.n.eric@gmail.com>
2020-04-23 15:17:10 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,36 @@
 # Add your code here
+
+class Dog
+    attr_accessor :name
+    @@all =[]
+
+    def initialize(dog_name)
+        @name = dog_name
+        save
+    end
+
+    def self.all
+        return @@all
+    end
+
+    def self.print_all
+        @@all.each do |doggo|
+            puts doggo.name
+        end
+    end
+
+    def self.clear_all
+        @@all = []
+    end
+
+    def save
+        @@all << self
+    end
+
+
+
+
+
+
+
+end
