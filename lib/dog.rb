b38c7c70dd01e720e1d70ad34c1ef7015a0fa8f1
estherkimyunjung <estherkimyunjung@gmail.com>
2020-04-26 22:34:14 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,19 +10,21 @@ class Dog
         #@save = save
     end
 
+    def self.all
+        @@all
+    end
+
     def self.print_all
         puts @@all.map{|dog| dog.name}
         # puts self.all.map{|dog| dog.name}
         # puts @@all.map(&:name)
     end
+
     def save
         @@all << self
     end
+
     def self.clear_all
         @@all.clear #self.all.clear
-    end
-
-    def self.all
-        @@all
     end
 end
